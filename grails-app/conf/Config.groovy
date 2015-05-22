0de748c27674e9e6c3cbc83c10bae92bd932ac95
marchah <marchah.dev@gmail.com>
2015-05-22 10:16:05 -0700

    get config property from externe file

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -2,6 +2,10 @@
 // config files can be ConfigSlurper scripts, Java properties files, or classes
 // in the classpath in ConfigSlurper format
 
+
+grails.config.locations = ["file:${userHome}/.grails/${appName}-config.groovy",
+                           "classpath:${appName}-config.groovy"]
+
 // grails.config.locations = [ "classpath:${appName}-config.properties",
 //                             "classpath:${appName}-config.groovy",
 //                             "file:${userHome}/.grails/${appName}-config.properties",
